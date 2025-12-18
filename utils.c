71b5fff945ba685b42b127019922fc10a9a8467d
magusvagus <magusvagus@proton.me>
2025-12-18 21:11:49 +0100

    fixed naming and readability of err window function

--- utils.c
+++ utils.c
@@ -15,57 +15,70 @@ timestamp()
 }
 
 void
-err_screen( WINDOW *window, char *err_msg) 
+err_screen( WINDOW *p_window, char *err_msg) 
 {
-	if (window == NULL) {
-		window = stdscr;
+	if (p_window == NULL) {
+		p_window = stdscr;
 	}
 
 	int w_max_y;
 	int w_max_x;
 
-	int w_center_y;
-	int w_center_x;
+	int w_root_y;
+	int w_root_x;
 
-	int errw_max_y;
-	int errw_max_x;
+	int pop_max_y;
+	int pop_max_x;
 
-	int errw_center_y;
-	int errw_center_x;
+	int pop_root_y;
+	int pop_root_x;
+
+	int pop_height;
+	int pop_width;
+	int pop_start_x;
+	int pop_start_y;
 
 	int err_msg_len;
 
-	// get length and width of terminal/ window
-	getmaxyx(window, w_max_y, w_max_x);
+	// get length and width of terminal/ or given window
+	getmaxyx(p_window, w_max_y, w_max_x);
 
-	w_center_y = w_max_y/2;
-	w_center_x = w_max_x/2;
+	// popup window diameters
+	pop_height = 8;
+	pop_width = w_max_x - 10;
+	pop_start_x = 5;
 
-	WINDOW *err_window;
-	err_window = newwin(w_center_y - 10, w_max_x - 10, w_center_y, 5);
-	box(err_window, 0,0);
+	// calculate root of window y, x
+	pop_start_y = (pop_max_y/2) - (pop_height/2);
+	w_root_x = w_max_x/2;
 
-	getmaxyx(err_window, errw_max_y, errw_max_x);
+
+	WINDOW *p_pop_win;
+	p_pop_win = newwin(pop_height, pop_width, pop_start_y, pop_start_x);
+	box(p_pop_win, 0,0);
+
+	getmaxyx(p_pop_win, pop_max_y, pop_max_x);
 	
-	errw_center_y = errw_max_y/2;
-	errw_center_x = errw_max_x/2;
+	pop_root_y = pop_max_y/2;
+	pop_root_x = pop_max_x/2;
 
 	err_msg_len = strlen(err_msg);
 
-	mvwprintw(err_window, errw_center_y-1, errw_center_x - err_msg_len/2, "%s",err_msg);
+	mvwprintw(p_pop_win, pop_root_y-1, pop_root_x - err_msg_len/2, "%s",err_msg);
 
-	wattron(err_window,A_REVERSE);
-	mvwprintw(err_window, errw_center_y+1, errw_center_x-3, "> OK <");
-	wattroff(err_window,A_REVERSE);
+	// print in reverse colors
+	wattron(p_pop_win,A_REVERSE);
+	mvwprintw(p_pop_win, pop_root_y+1, pop_root_x-3, "> OK <");
+	wattroff(p_pop_win,A_REVERSE);
 
 	int ch;
 
 	refresh();
-	wrefresh(err_window);
+	wrefresh(p_pop_win);
 
 	// close window if input id given
-	ch = wgetch(err_window);
-	wclear(err_window);
-	wrefresh(err_window);
-	delwin(err_window);
+	ch = wgetch(p_pop_win);
+	wclear(p_pop_win);
+	wrefresh(p_pop_win);
+	delwin(p_pop_win);
 }
