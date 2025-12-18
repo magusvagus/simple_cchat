91a4d694c2859fea62b5f45de7055e996018e04b
magusvagus <magusvagus@proton.me>
2025-12-18 23:01:48 +0100

    fixed issue where popup window didnt appear

--- utils.c
+++ utils.c
@@ -21,57 +21,62 @@ err_screen( WINDOW *p_window, char *err_msg)
 		p_window = stdscr;
 	}
 
-	int w_max_y;
-	int w_max_x;
+	// souce/ root window diameters
+	int wy_max;
+	int wx_max;
 
-	int w_root_y;
-	int w_root_x;
+	int wy_mid;
+	int wx_mid;
 
-	int pop_max_y;
-	int pop_max_x;
+	// popup window
+	int popy_max;
+	int popx_max;
 
-	int pop_root_y;
-	int pop_root_x;
+	int popy_mid;
+	int popx_mid;
 
 	int pop_height;
 	int pop_width;
-	int pop_start_x;
-	int pop_start_y;
+	int popy_start;
+	int popx_start;
 
 	int err_msg_len;
 
+	// getch input
+	int ch;
+
 	// get length and width of terminal/ or given window
-	getmaxyx(p_window, w_max_y, w_max_x);
+	getmaxyx(p_window, wy_max, wx_max);
 
 	// popup window diameters
 	pop_height = 8;
-	pop_width = w_max_x - 10;
-	pop_start_x = 5;
+	pop_width = wx_max - 10;
+	popx_start = 5;
 
-	// calculate root of window y, x
-	pop_start_y = (pop_max_y/2) - (pop_height/2);
-	w_root_x = w_max_x/2;
+	// calculate center of window y, x
+	popy_start = (wy_max/2) - (pop_height/2);
+	wx_mid = wx_max/2;
 
-
+	// create new popup window
 	WINDOW *p_pop_win;
-	p_pop_win = newwin(pop_height, pop_width, pop_start_y, pop_start_x);
+	p_pop_win = newwin(pop_height, pop_width, popy_start, popx_start);
 	box(p_pop_win, 0,0);
 
-	getmaxyx(p_pop_win, pop_max_y, pop_max_x);
+	getmaxyx(p_pop_win, popy_max, popx_max);
 	
-	pop_root_y = pop_max_y/2;
-	pop_root_x = pop_max_x/2;
+	// get middle point of popup win
+	popy_mid = popy_max/2;
+	popx_mid = popx_max/2;
 
 	err_msg_len = strlen(err_msg);
 
-	mvwprintw(p_pop_win, pop_root_y-1, pop_root_x - err_msg_len/2, "%s",err_msg);
+	// error message
+	mvwprintw(p_pop_win, popy_mid-1, popx_mid - err_msg_len/2, "%s",err_msg);
 
-	// print in reverse colors
+	// print ok in reverse colors
 	wattron(p_pop_win,A_REVERSE);
-	mvwprintw(p_pop_win, pop_root_y+1, pop_root_x-3, "> OK <");
+	mvwprintw(p_pop_win, popy_mid+1, popx_mid-3, "> OK <");
 	wattroff(p_pop_win,A_REVERSE);
-
-	int ch;
 
 	refresh();
 	wrefresh(p_pop_win);
