bc93c1b3c57cb5d000537b5579f4ada91f70f9f4
magusvagus <magusvagus@proton.me>
2026-01-01 16:35:34 +0100

    utils.h: added struct for sub window function

--- utils.h
+++ utils.h
@@ -3,6 +3,11 @@
 #ifndef UTILS_H
 #define UTILS_H
 
+struct Win_nested {
+	WINDOW* main;
+	WINDOW* sub;
+};
+
 struct tm* timestamp();
 void err_screen(WINDOW *p_window,char *win_title, char *err_msg);
 
