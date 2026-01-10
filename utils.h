7521158516ef40e1f8a152823b622c0062c337e2
magusvagus <magusvagus@proton.me>
2026-01-10 15:46:48 +0100

    utils.h: created Win_ui struct to hold ui elements

--- utils.h
+++ utils.h
@@ -9,6 +9,12 @@ struct Win_nested {
 	WINDOW* sub;
 };
 
+struct Win_ui {
+	struct Win_nested *send_win;
+	struct Win_nested *recv_win;
+	struct Win_nested *nick_win;
+};
+
 struct tm* timestamp();
 void err_screen(WINDOW *p_window,char *win_title, char *err_msg);
 struct Win_nested* win_nested(char *title, int winy, int winx, int drawpty, int drawptx, int wfl);
