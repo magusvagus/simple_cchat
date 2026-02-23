6d4ddd67fa4078d4747ecafad30df6d67eca41cc
magusvagus <magusvagus@proton.me>
2026-02-23 17:16:33 +0100

    utils.h -> created header for win_pad_nested function

--- utils.h
+++ utils.h
@@ -19,6 +19,7 @@ struct Win_ui {
 struct tm* timestamp();
 void win_errpopup(WINDOW *p_window,char *win_title, char *err_msg);
 struct Win_nested* win_nested(char *title, int winy, int winx, int drawpty, int drawptx, int wfl);
+struct Win_nested* win_pad_nested(char *title, int winy, int winx, int drawpty, int drawptx, int wfl);
 void win_reset(struct Win_ui *ui);
 void win_main_ui_init(struct Win_ui *ui);
 void win_login_ui_init(struct Win_ui *ui);
